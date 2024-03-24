5256df7da1f41ac78368aa70ade09ad1d26559ae
sky line <32512284+linesky@users.noreply.github.com>
2024-03-24 17:29:49 +0000

    Add files via upload

--- hello.asm
+++ hello.asm
@@ -1,21 +1,40 @@
 format mz
+           
+                          
 start:
-		
-          mov edi,120001h
-          mov ecx,80*26*2
-          mov al,017h
-          mov edx,2
-          call fill32
-          mov edi,120000h
-          mov ecx,80*26*2
-          mov al,'*'
-          mov edx,2
-          call fill32  
-          mov esi,120000h
-          mov edi,0b8000h
-          mov ecx,80*25*2
-          mov edx,1
-          call copymem32                        
+JMP MAIN                
+sourcetext dd 1
+destenytext dd 1
+x:    dd 0     
+y:     dd 0
+color dd 07h
+MAIN:   		
+          call clears
+          
+          mov si,text1
+          inc si
+          mov ax,cs
+          call mem32
+                          
+          mov esi,eax
+          mov ecx,16
+          mov ax,1                
+                          
+                          
+                    cs           
+                    mov [x],al                
+                    cs
+                    mov [y],al                
+                    call print32                           
+mov bx,endf
+mov cx,10
+call getstr
+mov bx,str2
+mov cx,13
+call cprint
+                    
+                    
+                     
                           
           exitdo:                
                                     
@@ -96,11 +115,283 @@ copymem32:
                     pop ecx                 
                     pop ebx                
                     pop eax                
-                    RET                       
+                    RET 
+mem32:                
+          push esi                
+                          
+          and eax,0ffffh
+          clc                
+          shl eax,4
+          and esi,0ffffh
+          clc                 
+          add eax,esi
+                          
+          pop esi                
+          RET 
+gotoxy:                
+          push ebx                
+          push ecx                
+          push edx                
+          push esi                
+          push edi                
+          push ebp                
+          mov si,ax
+          mov di,bx
+          and si,0fffh
+          and di,0fffh
+          xor cx,cx
+          xor dx,dx
+          mov ax,si
+          mov bx,2                
+          clc                
+          mul bx                
+          mov si,ax
+          mov ax,di
+          mov bx,160
+          clc                
+          mul bx                
+          mov bx,si
+          clc                
+          add ax,bx
+          and eax,0ffffh
+          mov ebx,0b8000h
+          clc                
+          add eax,ebx
+          pop ebp                
+          pop edi                
+          pop esi                
+          pop edx                
+          pop ecx                 
+          pop ebx                
+          RET                                       
 
-x     db 0     
-y     db 0
-color dw 07h
-string1 db "hello world.....",13,10,"$"
+print:                
+          push ax                
+          push bx                
+          push cx                
+          push dx                
+          push di                
+          push si                
+          push bp                
+          push es                
+          mov ax,cs
+          mov es,ax
+          xchg dx,bp 
+          mov bx,bp
+          cs
+          mov cl,[bx]
+          inc bp                
+          and cx,0ffh 
+          mov bx,x
+          cs
+          mov dx,[bx]
+          mov bx,color
+          cs
+          mov al,[bx]
+          mov bl,al
+          mov bh,0                
+          mov al,0                
+          mov ah,13h
+          int 10h                
+          pop es                
+          pop bp                
+          pop si                
+          pop di                
+          pop dx                
+          pop cx                
+          pop bx                
+          pop ax                
+          RET         
+clears:
+          push ax                
+          push bx                
+          push cx                
+          push dx                
+          push di                
+          push si                
+          push bp                
+          push es    
+          mov edi,0b8001h
+          mov ecx,80*26
+          mov al,017h
+          mov edx,2
+          call fill32
+          mov edi,0b8000h
+          mov ecx,80*26
+          mov al,32
+          mov edx,2
+          call fill32
+          pop es                
+          pop bp                
+          pop si                
+          pop di                
+          pop dx                
+          pop cx                
+          pop bx                
+          pop ax                
+          RET        
+
+print32:                
+          push eax                
+          push ebx                
+          push ecx                
+          push edx                
+          push esi                
+          push edi                
+          push ebp                
+          cmp ecx,0
+          JZ PRINT3213
+          cs
+          mov al,[x]                
+          cs
+          mov bl,[y]               
+          and ax,0ffh
+          and bx,0ffh
+          call gotoxy
+          mov edi,eax
+          cmp ecx,255
+          JB PRINT3212
+          mov ebx,255
+          PRINT3212:
+          mov edx,2
+          call copymem32 
+          cs
+          mov al,[x]                
+          cs
+          mov bl,[y]                
+          and ax,0ffh
+          and bx,0ffh
+          mov si,bx
+          clc                
+          add ax,cx
+          cmp ax,80
+          JB PRINT328
+          mov bx,80
+          sub ax,bx
+          xor dx,dx
+          xor cx,cx
+          mov bx,80
+          clc                
+          div bx                
+          clc                
+          add ax,si
+          cmp ax,24
+          JB PRINT328
+          mov ax,24
+          PRINT328:
+          cs
+          mov [y],ax                
+          cs
+          mov [x],dx 
+          PRINT3213:
+          pop ebp                
+          pop edi                
+          pop esi                
+          pop edx                
+          pop ecx                 
+          pop ebx                
+          pop eax                
+          RET                
+               
+scrollup:
+          push eax                
+          push ebx                
+          push ecx                
+          push edx                
+          push esi                
+          push edi                
+          push ebp                
+          push ds                
+          mov ax,0                
+          mov ds,ax
+          mov edi,0b8000h
+          mov esi,0b8000h+160  
+          mov ecx,80*24*2
+          mov edx,1
+          call copymem32
+          pop ds                
+          pop ebp                
+          pop edi                
+          pop esi                
+          pop edx                
+          pop ecx                 
+          pop ebx                
+          pop eax                
+          RET          
+
+printf:                
+          push ax                
+          push bx                
+          xor bh,bh
+          mov bl,70h
+          mov ah,0eh
+          int 10h                
+          pop bx                
+          pop ax                
+          RET                
+                
+                
+getchar:                
+          xor ax,ax
+          int 16h                
+          RET                
+                
+                
+getstr:                
+          push ax                
+          push bx                
+          push cx                
+          push dx                
+          mov dx,cx
+          GETSTR1:                
+                    call getchar
+                    cmp al,7                
+                    JNZ GETSTR2
+                    inc cx                
+                    dec bx                
+                    cmp cx,dx
+                    JB GETSTR2
+                    inc bx                
+                    mov cx,dx
+                    GETSTR2:                
+                    cmp al,13
+                    JZ GETSTR3
+                    mov [bx],al
+                    call printf
+                    inc bx                
+                    dec cx                
+                    cmp cx,0                
+                    JA GETSTR1 
+          GETSTR3:                
+          pop dx                
+          pop cx                
+          pop bx                
+          pop ax                
+          RET                
+                
+                
+                
+                
+cprint:                
+          push ax                
+          push bx                
+          push cx                
+          push dx                
+          CPRINT1:               
+                    mov al,[bx]
+                    call printf
+                    inc bx                
+                    dec cx                
+                    cmp cx,0                
+                    JNZ CPRINT1
+          pop dx                
+          pop cx                
+          pop bx                
+          pop ax                
+          RET     
+
+
+text1 db 22,"input a string.....",13,10,0
 string2 db "$",0
-
+str2 db 13,10,">"
+endf db "          .                                                               "
