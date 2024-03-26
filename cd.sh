e3b1cf67389a7c0ec2de30a43953daed4cb3c2ea
sky line <32512284+linesky@users.noreply.github.com>
2024-03-26 11:35:27 +0000

    Add files via upload

--- cd.sh
+++ cd.sh
@@ -11,10 +11,25 @@ cp *.elf CD_root/isolinux/
 cp *.jpg CD_root/isolinux/
 cp *.menu CD_root/isolinux/
 cp *.txt CD_root/isolinux/
-fasm hello.asm LOAD.EXE
-fasm mzload.asm hello.com
-cp LOAD.EXE CD_root/isolinux/
-cp *.c32 CD_root/isolinux/
+fasm cmd.asm hello.com
+fasm type.asm type.com
+fasm beep.asm beep.com
+fasm sound.asm sound.com
+fasm help.asm help.com
+fasm dir.asm dir.com
+fasm exit.asm exit.com
+fasm cls.asm cls.com
+fasm sleep.asm sleep.com
+fasm time.asm time.com
+fasm date.asm date.com
+fasm clock.asm clock.com
+fasm shutdown.asm shutdown.com
+fasm line.asm line.com
+fasm echo.asm echo.com
 cp *.com CD_root/isolinux/
+cp *.exe CD_root/isolinux/
+ls *.com CD_root/isolinux/ > list.txt
+sed -i 's/$/\r/g'  list.txt 
+cp list.txt CD_root/isolinux/list.txt
 genisoimage -o myos.iso -input-charset utf-8 -b isolinux/isolinux.bin -no-emul-boot -boot-load-size 4  -boot-info-table ./CD_root 
 
